a6d487b995fc9c58234af9ef335ef8fcd94f2508
zxl <zhangxl@danqoo.com>
2016-12-23 11:35:42 +0800

    ahab

--- Iris-app-jilu/src/main/java/org/Iris/app/jilu/storage/redis/cache/RelationCache.java
+++ Iris-app-jilu/src/main/java/org/Iris/app/jilu/storage/redis/cache/RelationCache.java
@@ -2,10 +2,8 @@ package org.Iris.app.jilu.storage.redis.cache;
 
 import javax.annotation.Resource;
 
-import org.Iris.app.jilu.common.bean.enums.RelationMod;
 import org.Iris.app.jilu.storage.domain.Relation;
 import org.Iris.app.jilu.storage.mybatis.mapper.RelationMapper;
-import org.Iris.app.jilu.storage.redis.CommonKeyGenerator;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,14 +12,8 @@ public class RelationCache extends RedisCache {
 	@Resource
 	private RelationMapper relationMapper;
 	
-	public boolean isFriend(String relationId) {
-		String modVal = redisOperate.hget(CommonKeyGenerator.relationMapKey(), relationId);
-		if (null == modVal) {
-			Relation relation = relationMapper.getById(relationId);
-			if (null != relation) {
-				redisOperate.hset(CommonKeyGenerator.relationMapKey(), relationId, String.valueOf(relation.getMod()));
-			}
-		}
-		return null == modVal ? false : RelationMod.isMod(Integer.valueOf(modVal), RelationMod.FRIEDN);
+	public Relation getRelationById(String relationId) {
+		Relation relation = getHashBean(new Relation(relationId));
+		return null;
 	}
 }
