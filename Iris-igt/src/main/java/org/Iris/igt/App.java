5ef5cd324eb0099c28d0eb1292388cc116ded21b
fansd <531273925@qq.com>
2017-01-24 17:28:28 +0800

    fsd

---
+++ Iris-igt/src/main/java/org/Iris/igt/App.java
@@ -0,0 +1,13 @@
+package org.Iris.igt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
