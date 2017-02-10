76e7368044b2d5f220a84d1b6f9afd2698362b20
fansd <531273925@qq.com>
2017-02-10 18:02:10 +0800

    fsd

---
+++ Iris-app-jilu/src/main/java/org/Iris/app/jilu/common/bean/enums/IgtPushType.java
@@ -0,0 +1,16 @@
+package org.Iris.app.jilu.common.bean.enums;
+/**
+ * 推送类型
+ * @author 樊水东
+ * 2017年2月10日
+ */
+public enum IgtPushType {
+
+	TRANSFORM(0);
+	
+	private int mark;
+	
+	private IgtPushType(int mark){
+		this.mark = mark;
+	}
+}
