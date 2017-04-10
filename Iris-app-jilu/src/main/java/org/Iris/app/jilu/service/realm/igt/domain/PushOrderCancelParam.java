e02eb680ed8fa94e307a5e85846989269fe9052c
fansd <531273925@qq.com>
2017-04-10 10:04:37 +0800

    fsd

--- Iris-app-jilu/src/main/java/org/Iris/app/jilu/service/realm/igt/domain/PushOrderCancelParam.java
+++ Iris-app-jilu/src/main/java/org/Iris/app/jilu/service/realm/igt/domain/PushOrderCancelParam.java
@@ -5,7 +5,8 @@ public class PushOrderCancelParam extends PushCommonParam{
 	private String orderId;
 	private String superOrderId;
 	public PushOrderCancelParam(String orderId, String superOrderId) {
-		super();
+		title = "订单取消";
+		content = "订单取消";
 		this.orderId = orderId;
 		this.superOrderId = superOrderId;
 	}
