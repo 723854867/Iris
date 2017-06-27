89d6144436a70c028e08a7e34822e3885c300605
fansd <531273925@qq.com>
2017-06-27 18:21:21 +0800

    fsd

--- Iris-app-jilu/src/main/java/org/Iris/app/jilu/service/action/web/DJH_LIST.java
+++ Iris-app-jilu/src/main/java/org/Iris/app/jilu/service/action/web/DJH_LIST.java
@@ -1,7 +1,9 @@
 package org.Iris.app.jilu.service.action.web;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.Iris.app.jilu.common.bean.form.AllOrderGoodsSumForm;
 import org.Iris.app.jilu.service.action.MerchantWebAction;
 import org.Iris.app.jilu.storage.domain.MemGoodsStore;
 import org.Iris.app.jilu.web.session.MerchantWebSession;
@@ -16,6 +18,14 @@ public class DJH_LIST extends MerchantWebAction{
 	@Override
 	protected String execute0(MerchantWebSession session) {
 		List<MemGoodsStore> list = memGoodsStoreMapper.getDjhMemGoodsStoreListByMerchantId(session.getMemMerchant().getMerchantId());
-		return Result.jsonSuccess(list);
+		List<AllOrderGoodsSumForm> djhList = new ArrayList<AllOrderGoodsSumForm>();
+		for(MemGoodsStore memGoodsStore : list){
+			AllOrderGoodsSumForm form = new AllOrderGoodsSumForm();
+			form.setGoodsName(memGoodsStore.getGoodsName());
+			form.setMerchantName(form.getMerchantName());
+			form.setCount(form.getCount());
+			djhList.add(form);
+		}
+		return Result.jsonSuccess(djhList);
 	}
 }
